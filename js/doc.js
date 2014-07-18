e543878e4c9c97e959fb609aeaa864e7e4834c19
Lynn <homlean@gmail.com>
2014-07-18 20:05:21 +0800

    Update doc.js

--- js/doc.js
+++ js/doc.js
@@ -1,5 +1,3 @@
 // jump
 
-// location.href = 'https://app.yinxiang.com/shard/s3/sh/ea4cb9d0-4cd7-48b2-9063-bc8ea0ddc2ab/a3173b70cbefbdc86aa0e1b70de89ab5';
-
-location.href = 'http://weibo.com/flickr001';
+location.href = 'https://app.yinxiang.com/shard/s3/sh/ea4cb9d0-4cd7-48b2-9063-bc8ea0ddc2ab/a3173b70cbefbdc86aa0e1b70de89ab5';
