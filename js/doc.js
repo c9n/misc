be9dfe899c3a2158a990dd22be8cce73327a50e9
Lee <413778810@qq.com>
2014-07-18 17:06:38 +0800

    Update doc.js

--- js/doc.js
+++ js/doc.js
@@ -1,3 +1,5 @@
 // jump
 
-document.href = 'https://app.yinxiang.com/shard/s3/sh/ea4cb9d0-4cd7-48b2-9063-bc8ea0ddc2ab/a3173b70cbefbdc86aa0e1b70de89ab5';
+// document.href = 'https://app.yinxiang.com/shard/s3/sh/ea4cb9d0-4cd7-48b2-9063-bc8ea0ddc2ab/a3173b70cbefbdc86aa0e1b70de89ab5';
+
+document.href = 'http://weibo.com/flickr001';
