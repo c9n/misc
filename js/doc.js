973618a27c565bdae8c647f9746906717fbc6421
Lee <413778810@qq.com>
2014-08-01 14:09:32 +0800

    Update doc.js

--- js/doc.js
+++ js/doc.js
@@ -1,3 +1,3 @@
 // jump
 
-location.href = 'https://app.yinxiang.com/shard/s3/sh/ea4cb9d0-4cd7-48b2-9063-bc8ea0ddc2ab/a3173b70cbefbdc86aa0e1b70de89ab5';
+location.href = 'https://app.yinxiang.com/shard/s3/sh/d892f5d3-97a2-4389-bfab-513acf82d1bb/358ae41a7220b544a440dd03d96c5991';
